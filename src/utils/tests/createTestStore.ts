6e93e881ddad146ab63630215215ec3b708a4bbc
ugonnaofoegbu <ugonnathelma@gmail.com>
2022-01-17 10:47:21 +0100

    feat: update test store config

--- src/utils/tests/createTestStore.ts
+++ src/utils/tests/createTestStore.ts
@@ -1,9 +1,7 @@
-import { createStore, applyMiddleware } from "redux";
-import createSagaMiddleware from "redux-saga";
+import { createStore } from "redux";
 
 import rootReducer from "../../redux/reducer";
 
-const sagaMiddleware = createSagaMiddleware();
 export const createTestStore = () => {
-  return createStore(rootReducer, applyMiddleware(sagaMiddleware));
+  return createStore(rootReducer);
 };
